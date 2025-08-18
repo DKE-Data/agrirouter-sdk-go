c30cc5cbcb06b25e035fe860a2724e339f78cc0f
Timur Sultanaev <t.sultanaev@reply.de>
2025-08-18 19:24:05 +0200

    feat: expose endpoint types

--- models.go
+++ models.go
@@ -42,3 +42,23 @@ const CapabilityDirectionReceive = internal_models.RECEIVE
 
 // CapabilityDirectionSendReceive indicates capability of endpoint to both send and receive messages.
 const CapabilityDirectionSendReceive = internal_models.SENDRECEIVE
+
+// CommunicationUnit is an endpoint type representing a communication unit.
+//
+// Communication units are typically devices that can send and receive agrirouter messages,
+// which are installed inside of a vehicle or a machine.
+const CommunicationUnit = internal_models.CommunicationUnit
+
+// VirtualCommunicationUnit is an endpoint type representing a virtual communication unit.
+//
+// Virtual communication units like usual communication units represent devices
+// that can send and receive agrirouter messages, but they are doing so indirectly
+// via their own cloud service, which is not installed inside of a vehicle or a machine.
+const VirtualCommunicationUnit = internal_models.VirtualCommunicationUnit
+
+// FarmingSoftware is an endpoint type representing farming software applications.
+//
+// Farming software applications can send and receive agrirouter messages,
+// and they are typically cloud deployed applications that manage agricultural data
+// and provide farmers with their own typically web-based user interface.
+const FarmingSoftware = internal_models.FarmingSoftware
